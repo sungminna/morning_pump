9d2191a1e2e506644efa3491a3689bb90b0964d8
sungminna <sungmin.na330@gmail.com>
2021-07-28 22:36:48 +0900

    next time control shortened, able to get data by dataframe(9 oclock) from sqlite db

--- research/backtesting.py
+++ research/backtesting.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import math
 import sqlite3
+import time
 
 
 class Backtesting():
@@ -11,14 +13,19 @@ class Backtesting():
         self.conn = sqlite3.connect("C:/Users/DEV/Desktop/coin_database/60min.db")
         self.cur = self.conn.cursor()
         self.existing_table = list()
+        self.how_many_days = int()
 
         #####
         self.get_table_from_db()
 
+        self.cur.execute("SELECT count(*) FROM table_BTC;")
+        item = self.cur.fetchone()
+        self.how_many_days = math.floor((int(item[0])/24))
+        print(self.how_many_days)
+
         for name in self.existing_table:
             name = name[-3:]
             self.parse_data(name)
-
 
 
     def get_table_from_db(self):
@@ -35,20 +42,37 @@ class Backtesting():
         query = "SELECT candle_date_time_kst FROM table_" + name + ";"
         self.cur.execute(query)
         item = self.cur.fetchone()
-        Y = item[0][:4]
-        M = item[0][5:7]
-        D = item[0][8:10]
-        print(item[0])
+        working_time = item[0][:10] + "T09:00:00"
+        working_time = self.next_time(working_time)
+
+        temp_list = list()
+
+        for _ in range(self.how_many_days):
+            query = "SELECT candle_date_time_kst, opening_price, high_price, low_price, trade_price FROM table_" + name + " WHERE candle_date_time_kst = '" + working_time + "';"
+            self.cur.execute(query)
+            item = self.cur.fetchone()
+            if item == None:
+                pass
+            else:
+                item = list(item)
+                temp_list.append(item)
+
+
+            working_time = self.next_time(working_time)
+
+
+        df = pd.DataFrame(temp_list, columns=['candle_date_time_kst', 'opening_price', 'high_price', 'low_price', 'trade_price'])
+        print(df)
+        print("====")
+        time.sleep(10)
+            #latest = item[-1]
+            #self.latest_db = str(latest).strip('(,\)')
+
+    def next_time(self, time):
+        Y = int(time[:4])
+        M = int(time[5:7])
+        D = int(time[8:10])
         print(Y, M, D)
-
-        query = "SELECT candle_date_time_kst, opening_price, high_price, low_price, trade_price FROM table_" + name + "WHERE candle_date_time_kst == '" + time + "';"
-
-        self.cur.execute(query)
-        item = self.cur.fetchall()
-        #latest = item[-1]
-        #self.latest_db = str(latest).strip('(,\)')
-
-    def next_time(self, Y, M, D):
         #다음 시간
         if(M in[1, 3, 5, 7, 8, 10]):
             if(D==31):
@@ -75,8 +99,14 @@ class Backtesting():
                 D = 1
             else:
                 D += 1
+        ## 0 추가
+        if M<10:
+            M = "0" + str(M)
+        if D<10:
+            D = "0" + str(D)
 
-        return(Y, M, D)
+        rtn = str(Y) + "_" + str(M) + "_" + str(D) + "T09:00:00"
+        return(rtn)
 
 
 if __name__ == "__main__":
